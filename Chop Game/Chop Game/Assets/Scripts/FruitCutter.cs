c4a53965c648acdf961ce137d4a9708e013095ff
mdhurpraksh <67580872+mdhurpraksh@users.noreply.github.com>
2021-12-12 05:49:12 +0530

    Food cutter

---
+++ Chop Game/Chop Game/Assets/Scripts/FruitCutter.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitCutter : MonoBehaviour
+{
+
+    [SerializeField] GameObject cutFruit;
+    [SerializeField] float force;
+   [SerializeField] float explosionRadius;
+
+
+    GameObject spawnedFood;
+    Rigidbody[] spawnedFoodRb;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision obj)
+    {
+
+        if (obj.gameObject.tag == "Knife")
+        {
+
+             spawnedFood = Instantiate(cutFruit, transform.position, transform.rotation);
+             spawnedFoodRb = spawnedFood.GetComponentsInChildren<Rigidbody>();
+
+            foreach(Rigidbody rb2 in spawnedFoodRb)
+            {
+                rb2.AddExplosionForce(force,spawnedFood.transform.position,explosionRadius);
+            }
+
+
+
+
+            Destroy(gameObject, 0);
+
+           
+        }
+    }
+}
