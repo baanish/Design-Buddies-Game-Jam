620344487ca8eb6ae0f8e607c9acd1c205813213
mdhurpraksh <67580872+mdhurpraksh@users.noreply.github.com>
2021-12-13 16:51:42 +0530

    food health

--- Chop Game/Chop Game/Assets/Scripts/FruitCutter.cs
+++ Chop Game/Chop Game/Assets/Scripts/FruitCutter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FruitCutter : MonoBehaviour
 {
@@ -8,20 +9,25 @@ public class FruitCutter : MonoBehaviour
     [SerializeField] GameObject cutFruit;
     [SerializeField] float force;
     [SerializeField] float explosionRadius;
-    
+    [SerializeField] int fruitHealth;
+    [SerializeField] GameObject healthImage;
+    Slider slider;
 
 
     GameObject spawnedFood;
     Rigidbody[] spawnedFoodRb;
 
+    float sliderSubstractValue;
+
     void Start()
     {
-        
+        slider = healthImage.GetComponent<Slider>();
+        sliderSubstractValue = slider.value / fruitHealth;
     }
 
     void Update()
     {
-        
+       
     }
 
     private void OnCollisionEnter(Collision obj)
@@ -30,16 +36,24 @@ public class FruitCutter : MonoBehaviour
         if (obj.gameObject.tag == "Knife")
         {
 
-             spawnedFood = Instantiate(cutFruit, transform.position, transform.rotation);
-             spawnedFoodRb = spawnedFood.GetComponentsInChildren<Rigidbody>();
+            
 
-            foreach(Rigidbody rb2 in spawnedFoodRb)
+            if (fruitHealth == 1)
             {
-                rb2.AddExplosionForce(force,spawnedFood.transform.position,explosionRadius);
+                spawnedFood = Instantiate(cutFruit, transform.position, transform.rotation);
+                spawnedFoodRb = spawnedFood.GetComponentsInChildren<Rigidbody>();
+
+                foreach (Rigidbody rb2 in spawnedFoodRb)
+                {
+                    rb2.AddExplosionForce(force, spawnedFood.transform.position, explosionRadius);
+                }
+
+                Destroy(gameObject, 0);
+                
             }
 
-            Destroy(gameObject, 0);
-
+            slider.value -= sliderSubstractValue;
+            fruitHealth -= 1;
 
         }
 
